dad0724957448bbb0893a31cecdef76943f0b19f
Shivani Kamble <shivanikamble083@gmail.com>
2024-11-25 18:06:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,14 +50,12 @@ function App() {
 	if (isCheckingAuth) return <LoadingSpinner />;
 
 	return (
-		<div
-			className='min-h-screen bg-gradient-to-br
-    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
-		>
-			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
-			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
-
+		  <div
+			className='min-h-screen bg-gradient-to-r from-black via-gray-900 to-black h-64 w-full flex w- items-center justify-center relative overflow-hidden'>
+		<FloatingShape color='bg-gray-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+    <FloatingShape color='bg-gray-500' size='w-48 h-48' top='60%' left='60%' delay={5} />
+    <FloatingShape color='bg-gray-500' size='w-32 h-32' top='40%' left='20%' delay={2} />
+     <FloatingShape color='bg-gray-500' size='w-72 h-72' top='20%' left='50%' delay={3} />
 			<Routes>
 				<Route
 					path='/'
