66aca658fd9683faef27393e2688469791ff6e81
Shivani Kamble <shivanikamble083@gmail.com>
2024-11-25 14:13:37 +0530

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -23,6 +23,11 @@ const LoginPage = () => {
 			transition={{ duration: 0.5 }}
 			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
 		>
+			<style>
+		   .hidden {
+                      display: none;
+    			}
+		</style>
 			<div className='p-8'>
 				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
 					Welcome Back
