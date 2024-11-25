0a1daad8631e1c9e06dabb9c720be22b28660a46
Shivani Kamble <shivanikamble083@gmail.com>
2024-11-25 18:02:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,15 +17,15 @@ import { useEffect } from "react";
 // protect routes that require authentication
 const ProtectedRoute = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
-        if (isAuthenticated && user.isVerified) {
-		return <Navigate to='/Dashboard' replace />;}
+
 	if (!isAuthenticated) {
 		return <Navigate to='/login' replace />;
 	}
 
 	if (!user.isVerified) {
-		 <Navigate to='/verify-email' replace />;
+		return <Navigate to='/verify-email' replace />;
 	}
+
 	return children;
 };
 
@@ -34,39 +34,33 @@ const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
 
 	if (isAuthenticated && user.isVerified) {
-		return <Navigate to='/Dashboard' replace />;
+		return <Navigate to='/' replace />;
 	}
 
 	return children;
 };
 
 function App() {
-  const { isCheckingAuth, checkAuth } = useAuthStore();
+	const { isCheckingAuth, checkAuth } = useAuthStore();
 
 	useEffect(() => {
 		checkAuth();
 	}, [checkAuth]);
 
 	if (isCheckingAuth) return <LoadingSpinner />;
-  return (
-    <>
-    <div
-			className='min-h-screen bg-gradient-to-r from-black via-gray-900 to-black h-64 w-full flex w- items-center justify-center relative overflow-hidden'>
-		<FloatingShape color='bg-gray-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-    <FloatingShape color='bg-gray-500' size='w-48 h-48' top='60%' left='60%' delay={5} />
-    <FloatingShape color='bg-gray-500' size='w-32 h-32' top='40%' left='20%' delay={2} />
-     <FloatingShape color='bg-gray-500' size='w-72 h-72' top='20%' left='50%' delay={3} />
-     <Routes>
+
+	return (
+		<div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+			<Routes>
 				<Route
 					path='/'
-					element={
-						<ProtectedRoute>
-							<DashboardPage />
-						</ProtectedRoute>
-					}
-				/>
-	                       <Route
-					path='/Dashboard'
 					element={
 						<ProtectedRoute>
 							<DashboardPage />
@@ -110,9 +104,9 @@ function App() {
 				{/* catch all routes */}
 				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
-      <Toaster />
-    </div>
-    </>
-  )
+			<Toaster />
+		</div>
+	);
 }
-export default App
+
+export default App;
