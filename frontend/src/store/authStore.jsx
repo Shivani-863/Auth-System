6036ddf4b92969a5fd4e18b70078661200a7a365
Shivani Kamble <shivanikamble083@gmail.com>
2024-11-27 16:39:57 +0530

    Update authStore.jsx

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -52,9 +52,15 @@ export const useAuthStore = create((set) => ({
 	verifyEmail: async (code) => {
 		set({ isLoading: true, error: null });
 		try {
-			const response = await axios.post(`${API_URL}/verify-email`, { code });
-			set({ user: response.data.user, isAuthenticated: true, isLoading: false });
-			return response.data;
+		 setTimeout(async () => {
+		                    const response = await axios.post(`${API_URL}/verify-email`, { code });
+                                    set({
+                                           user: response.data.user,
+                                           isAuthenticated: true,
+                                           isLoading: false,
+                                        });
+	         	              // return response.data; 
+                                        },600000);
 		} catch (error) {
 			set({ error: error.response.data.message || "Error verifying email", isLoading: false });
 			throw error;
